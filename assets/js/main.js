98442bad4f0db5123cb2008d1168387e071bc074
Aurélien Lorence <aurelien@keyframe.fr>
2023-07-05 17:16:20 +0200

    Cookies consent integration

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+const cookieStorage = 'cookiesChoice'
+
 function toggleMenu () {
     const menu = document.querySelector('.header__nav')
     if(menu.classList.contains('open')) {
@@ -8,3 +10,25 @@ function toggleMenu () {
     }
 
 }
+
+function displayCookiesRequest() {
+    fetch('partials/cookies.html')
+        .then(result => result.text())
+        .then(content => {
+            const placeHolder = document.createElement('div')
+            placeHolder.innerHTML = content
+            document.body.append(placeHolder.firstElementChild)
+        })
+}
+
+function setCookiesChoices() {
+    localStorage.setItem(cookieStorage, 'true')
+    document.getElementById('cookies-request').remove()
+}
+document.addEventListener('DOMContentLoaded', function () {
+    let storageChoice = localStorage.getItem(cookieStorage)
+
+    if(storageChoice == null) {
+        displayCookiesRequest()
+    }
+})
